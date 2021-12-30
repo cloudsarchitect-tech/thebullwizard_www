f26c3f914f6d67e520333128fa4e76da63fedce1
bibhas44 <bibhasghoshal44.bg@gmail.com>
2021-12-31 01:45:59 +0530

    feat(): complete installation of bootstrap and basic routing

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {NgbPanelChangeEvent} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'The Bull Wizard';
+  public beforeChange($event: NgbPanelChangeEvent) {
+
+    if ($event.panelId === 'preventchange-2') {
+      $event.preventDefault();
+    }
+
+    if ($event.panelId === 'preventchange-3' && $event.nextState === false) {
+      $event.preventDefault();
+    }
+  }
 }
