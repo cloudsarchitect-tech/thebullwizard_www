8acccb7fb0e555e0a30b0ca3d86dd99c9920e5db
bibhas44 <bibhasghoshal44.bg@gmail.com>
2021-12-31 02:56:37 +0530

    feat(): fixed header and footer

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -8,14 +8,5 @@ import {NgbPanelChangeEvent} from '@ng-bootstrap/ng-bootstrap';
 })
 export class AppComponent {
   title = 'The Bull Wizard';
-  public beforeChange($event: NgbPanelChangeEvent) {
 
-    if ($event.panelId === 'preventchange-2') {
-      $event.preventDefault();
-    }
-
-    if ($event.panelId === 'preventchange-3' && $event.nextState === false) {
-      $event.preventDefault();
-    }
-  }
 }
