f9589c3e42340d24d0dbdf39410edcfd69cda226
Bibhas Ghoshal <bibhasghoshal44.bg@gmail.com>
2022-11-14 23:57:35 +0530

    changes (#22)
    * fix():changes 001
    
    * fix():changes as per requirment

---
+++ app/src/app/side-icons/side-icons.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-side-icons',
+  templateUrl: './side-icons.component.html',
+  styleUrls: ['./side-icons.component.css']
+})
+export class SideIconsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
